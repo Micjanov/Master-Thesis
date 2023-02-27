460c7dba8ac005f0e63f41f025ebe8ea79b86b6c
Micjanov <mfatjanov@gmail.com>
2023-02-27 15:21:21 +0100

    restructure code folders + start embeddings

---
+++ src/HDC.jl
@@ -0,0 +1,60 @@
+using Random
+using LinearAlgebra
+"""
+Construct a binary vector. By default 10000 elements long.
+"""
+bithdv(N::Int=10000) = bitrand(N)
+"""
+Bundles binary hyperdimensional vectors based on the element-wise majority rule.
+"""
+function bitadd(vectors::BitVector ...)
+    v = reduce(.+, vectors)
+    n = length(vectors) / 2
+    x = [i > n ? 1 : i < n ? 0 : rand(0:1) for i in v]
+    return convert(BitVector, x)
+end
+"""
+Binds binary hyperdimensional vectors based on an element-wise XOR gate.
+"""
+bitbind(vectors::BitVector ...) =  reduce(.⊻, vectors)
+"""
+Permutes a binary hyperdimensional vector by an adjustable circular shift.
+"""
+bitperm(vector::BitVector, k::Int=1) = circshift(vector, k)
+"""
+Calculates the Hamming distance between two binary vectors.
+"""
+hamming(x::BitVector, y::BitVector) = sum(x .!= y)/length(x)
+"""
+Construct a bipolar vector. By default 10000 elements long.
+"""
+hdv(N::Int=10000) = rand!(zeros(N), (-1,1))
+"""
+Bundles bipolar hyperdimensional vectors.
+"""
+add(vectors...) = reduce(.+, vectors) .|> sign
+"""
+Binds binpolar hyperdimensional vectors.
+"""
+bind(vectors...) = reduce(.*, vectors)
+"""
+Permutes a bipolar hyperdimensional vector by an adjustable circular shift.
+"""
+perm(vector::Vector, k::Int=1) = circshift(vector, (0, k))
+"""
+Calculates the cosine similarity between two bipolar vectors.
+"""
+cosine(x::Vector, y::Vector) = dot(x, y) / (norm(x) * norm(y))
+"""
+Creates a list of bipolar hyperdimensional vectors representing an interval of numbers by constructing a random HDV representing the lower bound of the interval and replacing a fraction of the vector with random bits.
+"""
+function range_hdvs(steps)
+    k = length(steps) - 1
+    V = [hdv() for i in 1:k+1]
+    for i in 2:k+1
+        for j in 1:10000
+            V[i][j] = rand((-1, 1))
+        end
+    end
+    return V
+end
---
+++ src/math.jl
@@ -0,0 +1,25 @@
+
+"""
+Transform numbers in an array or 2D matrix to fit in a given range.
+"""
+function scaler(row, lower, upper)
+    minx = minimum(row)
+    maxx = maximum(row)
+    x = [lower + ((i - minx)*(upper-lower))/(maxx - minx) for i in row]
+    return x
+end
+
+function mat_scaler(matrix, lower, upper, dim = 1)
+    if dim == 2
+        scaled = reduce(hcat, [scaler(matrix[:, i], lower, upper) for i in 1:size(matrix, 2)])
+    elseif dim == 1
+        scaled = permutedims(hcat([scaler(matrix[i, :], lower, upper) for i in 1:size(matrix, 1)]...))
+    end
+    return scaled
+end
+"""
+Convert an array of arrays into a matrix
+"""
+function nested_arrays2mat(arrays)
+    return reduce(hcat,arrays)
+end
